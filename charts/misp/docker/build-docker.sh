b1002ed687feb8821c0eda64c50fca9da5e9bf38
Mark Priest <mpriest@sei.cmu.edu>
2024-12-11 11:33:07 -0500

    Remove docker experiment and add ignore for mac junk files

--- charts/misp/docker/build-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t mpriest/ubuntu-test .
--- charts/misp/docker/run-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm -p 8080:80 -p 443:443 -v misp:/misp -v misp-install:/var/log --name ubuntu-test mpriest/ubuntu-test
